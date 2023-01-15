8e1f810bc28c66eb657b7083a64243577169f344
Tom Quinn <tom@tquinn.uk>
2023-01-15 21:26:59 +0000

    remove lua extension

--- setup.lua
+++
@@ -1,7 +0,0 @@
--- setup
-if not fs.exists("loader/") then
-    shell.run("mkdir", "loader")
-    shell.setDir("loader")
-    shell.run("wget", "https://tomquinn04.github.io/ccprojects/loader/loader.lua")
-    shell.run("loader")
-end
