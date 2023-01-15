5a592d527f931661d621feb3417e3c3ae4d4f92c
Tom Quinn <tom@tquinn.uk>
2023-01-15 19:17:51 +0000

    fix url

--- setup.lua
+++ setup.lua
@@ -2,6 +2,6 @@
 if not fs.exists("loader/") then
     shell.run("mkdir", "loader")
     shell.setDir("loader")
-    shell.run("wget", "https://https://raw.githubusercontent.com/tomquinn04/ccprojects/main/loader/loader.lua")
+    shell.run("wget", "https://raw.githubusercontent.com/tomquinn04/ccprojects/main/loader/loader.lua")
     shell.run("loader")
 end
